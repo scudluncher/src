3fc155ace28fa1889a7877a419e471d7448727b6
scudluncher <scudluncher@gmail.com>
2021-01-29 01:10:45 +0900

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
+//코딩테스트 연습 해시  전화번호 목록 도움말 프로그래머스 
+
 class Solution1 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
