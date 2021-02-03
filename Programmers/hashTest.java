28a9305208a4a11a93e6a7380477960cc0917bc8
yjshin <scudluncher@gmail.com>
2021-02-03 09:10:14 +0900

    update!

---
+++ Programmers/hashTest.java
@@ -0,0 +1,8 @@
+
+
+public class hashTest {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ programmersTest1.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class programmersTest1 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Map<String,String> testArea = new HashMap<String,String>();
+       
+        
+        outer:for (String a : phone_book){
+            testArea.put(a,a);
+            for(String target : phone_book){
+                if(!a.equals(target)&&target.length()>a.length()){
+                    if(testArea.containsKey(target.substring(0,a.length()))){
+                        answer = false;
+                        break outer;
+                    }
+                }
+            }
+                
+        }
+        
+        
+        
+        return answer;
+}
