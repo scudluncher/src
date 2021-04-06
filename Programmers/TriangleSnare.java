0ef0387b9783be9cedcd07ee0d2cd7a379c60577
yjshin <scudluncher@gmail.com>
2021-04-06 18:16:34 +0900

    아몰랑

--- Programmers/TriangleSnare.java
+++ Programmers/TriangleSnare.java
@@ -13,59 +13,66 @@ public class TriangleSnare {
 
     public static int[] solution(int n) {
         int[] answer = {};
-
+        int limit = (1+n)*n/2;
         if (n==1){
             answer = new int[1];
             answer[0]=1;
             return answer;
+        }else{
+            answer = new int[limit];
         }
         int[][] matrix = new int[n][n];
-        int limit = (1+n)*n/2;
-
+       
 
         //vertical -> horizontal -> diagonal -> vertical -> horizontal -> diagonal ...
         verticalFill(0,0,n,matrix, limit);
-        
+        int idx = 0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0 ; j< matrix[i].length;j++){
+                if(matrix[i][j]!=0){
+                    answer[idx++] =matrix[i][j];
+                }
+            }
+        }
+
+        for(int k : answer){
+            System.out.println(k);
+        }
+
         return answer;
     }
 
 
     public static void verticalFill(int columnIdx,int start, int end, int[][] matrixParam, int limit){
-        for(int i=start ; i<end; i++){
-            matrixParam[i][columnIdx] = ++value;
+        while(matrixParam[start][columnIdx]!=0){
+            matrixParam[start++][columnIdx] = ++value;
             if(value==limit){
                 return;
             }
         }
-        horizontalFill( end-1, //ok
-                        start-horizontalCycle++,end,matrixParam,limit);  // row 3 = end -1 = 4-1  //   start 2 
+        horizontalFill( start , //ok length n-1 
+                      columnIdx+1, end-horizontalCycle++ ,matrixParam,limit);  // row 3 = end -1 = 4-1  //   start 2 
+       
     }
 
-    public static void horizontalFill(int rowIdx, int start, int end, int[][] matrixParam, int limit){
-        
-        for(int i=start+1 ; i <end; i++){
-            matrixParam[rowIdx][i] = ++value;
+    public static void horizontalFill(int rowIdx, int startY, int end, int[][] matrixParam, int limit){
+        while(matrixParam[rowIdx][startY++]!=0){
+            matrixParam[rowIdx][startY++] = ++value;
             if(value==limit){
                 return;
             }
         }
-        diagonalFill( end-2, diagonalCycle++, matrixParam,limit);
+        diagonalFill( rowIdx-1, , matrixParam,limit);
     }
 
-
-    public static void diagonalFill(int start, int end, int[][] matrixParam, int limit){
-        
-        for(int i=start ; i >end; i--){
-            matrixParam[i][i] = ++value;
+    public static void diagonalFill(int startX, int startY, int end, int[][] matrixParam, int limit){
+        while(matrixParam[startX][startY]!=0){
+            matrixParam[startX--][startY--] = ++value;
             if(value==limit){
                 return;
             }
-            if(i==end+1){
-            verticalFill(i,i+1,start+1,matrixParam,limit); //  n= 4  i = 1, start = 2, end = 
-                                                            //   n =5 i = 1, start = 3 , end = 0  
-            // verticalFill(i,   ,end-1,matrixParam,limit);
-            }
         }
+        verticalFill(i,i+1,start+1,matrixParam,limit); 
 
 
       //종료 체크
